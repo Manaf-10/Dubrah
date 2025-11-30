9d20d3d8434fda0913dfcd8e6a8b513ef55fdb50
BP-36-201-21 <bp-36-201-21@BP-36-201-21s-iMac.local>
2025-11-30 08:59:02 +0300

    xcode file

---
+++ Dubrah/Dubrah/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dubrah
+//
+//  Created by BP-36-201-21 on 30/11/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
