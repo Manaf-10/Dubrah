9b55686a2531af0dfc46a19be6ea9a22e0b615d9
Mohamed Kadhem <202304184@student.polytechnic.bh>
2025-12-03 03:59:55 +0800

    Main update

--- Dubrah/Dubrah/ViewController.swift
+++ Dubrah/Dubrah/ViewController.swift
@@ -9,9 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var GetStartedButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        GetStartedButton.layer.cornerRadius = 12.0
+        GetStartedButton.clipsToBounds = true
     }
 
 
