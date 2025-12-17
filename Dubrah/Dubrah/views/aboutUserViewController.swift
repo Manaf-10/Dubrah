e379f0da4464da928c0f747f336580ec04916ea7
Mohamed Kadhem <202304184@student.polytechnic.bh>
2025-12-17 16:55:44 +0800

    update Registration storyboard

---
+++ Dubrah/Dubrah/views/aboutUserViewController.swift
@@ -0,0 +1,25 @@
+//
+//  aboutUserViewController.swift
+//  Dubrah
+//
+//  Created by user282253 on 12/17/25.
+//
+
+import UIKit
+
+class aboutUserViewController: UIViewController  {
+  
+    
+ 
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+        
+        
+    }
+    
+    
+
+}
