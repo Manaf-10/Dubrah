b85f1028a3031ef92e142d7a63cbec2913657563
Sayed <s.h@192-168-100-133.rev.bb.zain.com>
2025-12-22 23:18:03 +0300

    added the notification model and the notification array in the user

---
+++ Dubrah/Dubrah/models/Notification.swift
@@ -0,0 +1,13 @@
+//
+//  Notification.swift
+//  Dubrah
+//
+//  Created by Sayed on 22/12/2025.
+//
+import Foundation
+
+struct Notification {
+    var content: String
+    var createdAt: Date
+    var senderImage: String
+}
