b2da5d8f9c6b32a6061d302fa56cfbc7f2e37baa
Mohamed Kadhem <202304184@student.polytechnic.bh>
2025-12-25 03:44:58 +0800

    update the registration and profile

---
+++ Dubrah/Dubrah/views/UserInfoCompleteViewController.swift
@@ -0,0 +1,20 @@
+//
+//  UserInfoCompleteViewController.swift
+//  Dubrah
+//
+//  Created by user282253 on 12/25/25.
+//
+
+import UIKit
+
+class UserInfoCompleteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+}
