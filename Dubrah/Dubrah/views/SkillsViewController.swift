5ad2dbe25d160e32439b86b53668ba895601d295
Mohamed Kadhem <202304184@student.polytechnic.bh>
2025-12-25 01:18:31 +0800

    Update skills and interests page

---
+++ Dubrah/Dubrah/views/SkillsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SkillsViewController.swift
+//  Dubrah
+//
+//  Created by user282253 on 12/24/25.
+//
+
+import UIKit
+
+class SkillsViewController: UIViewController{
+    
+
+    @IBOutlet weak var TableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+    
+
+   
+
+}
