8e19ec112e43a7fd545254da9143f06c259a8dc1
M07ammedAli <202304108@student.polytechnic.bh>
2026-01-03 14:56:58 +0300

    Update Service.swift

--- Dubrah/Dubrah/models/Service.swift
+++ Dubrah/Dubrah/models/Service.swift
@@ -1,20 +1,40 @@
-//
-//  Service.swift
-//  Dubrah
-//
-//  Created by BP-19-131-07 on 29/12/2025.
-//
-
 import Foundation
+import FirebaseFirestore
 
 struct Service {
     let id: String
-    let category: String
-    let description: String
-    let duration: Int
-    let image: String
-    let providerID: String
-    let title: String
-    let price: Double
-    let rating: Double
+    var category: String
+    var title: String
+    var description: String
+    var price: Double
+    var duration: Int
+    var image: String
+    var providerID: String
+    var paymentMethods: [String]
+    var createdAt: Date
+    var reviews: [Review]
+
+    init?(document: QueryDocumentSnapshot) {
+        let data = document.data()
+        
+        self.id = document.documentID
+        self.category = data["category"] as? String ?? ""
+        self.title = data["title"] as? String ?? ""
+        self.description = data["description"] as? String ?? ""
+        self.price = data["price"] as? Double ?? 0.0
+        self.duration = data["duration"] as? Int ?? 0
+        self.image = data["image"] as? String ?? ""
+        self.providerID = data["providerID"] as? String ?? ""
+        self.paymentMethods = data["paymentMethod"] as? [String] ?? []
+        let reviewData = data["reviews"] as? [[String: Any]] ?? []
+        self.reviews = reviewData.compactMap { Review(dictionary: $0)}
+        self.duration = data["duration"] as? Int ?? 0
+        
+        // Handle Firestore Timestamp conversion
+        if let timestamp = data["createdAt"] as? Timestamp {
+            self.createdAt = timestamp.dateValue()
+        } else {
+            self.createdAt = Date()
+        }
+    }
 }
